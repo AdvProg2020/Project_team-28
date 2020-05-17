ce67b8a58cd563988d9522d8aa5f81b9d8ef761e
morabba <292.arma@gmail.com>
2020-05-17 21:30:37 +0430

    starting ProductsMenu

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -7,7 +7,7 @@ public class ProductController extends UserController {
     public static String showProduct(String productId) {
         Product product = Database.getProductById(productId);
         assert product != null;
-        Seller seller = (Seller)Database.getUserById(product.getSeller());
+        Seller seller = (Seller) Database.getUserById(product.getSeller());
         assert seller != null;
         return "Product name:\t" + product.getName() + "\n" +
                 "Brand:\t" + product.getBrand() + "\n" +
@@ -16,5 +16,39 @@ public class ProductController extends UserController {
                 "In stock:\t" + (product.isInStock() ? "Yes" : "No") + "\n" +
                 "Average Score:\t" + product.getAverageScore() +
                 "Description:\n" + product.getDescription();
+    }
+
+    //TODO complete function
+
+    public String getCategories() {
+        return "These are all categories";
+    }
+
+    public String showAvailableFilters() {
+        return "These are all filters";
+    }
+
+    public String filterProducts(String filter) {
+        return "These are products after being filtered";
+    }
+
+    public String getCurrentFilters() {
+        return "These are current filters";
+    }
+
+    public void removeFilter(String filter) throws Exception {
+        throw new Exception("willing to remove filter " + filter);
+    }
+
+    public String showAvailableSorts() {
+        return "These are all sorts";
+    }
+
+    public void setSort(String sort) throws Exception {
+        throw new Exception("sort is set to " + sort);
+    }
+
+    public String getCurrentSort() {
+        return "This is the current sort";
     }
 }
---
+++ src/main/java/view/product/ProductsMenu.java
@@ -0,0 +1,67 @@
+package view.product;
+
+import controller.ProductController;
+import model.Product;
+import view.Menu;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+
+public class ProductsMenu extends Menu {
+
+    ProductController controller;
+
+    public ProductsMenu(ProductController productController) {
+        super();
+        controller = productController;
+        hint = "You can:\n";
+        title = "Who's The Boss?";
+        fortune = "You truly have tremendous charisma";
+        do {
+            show();
+        } while (execute());
+    }
+
+    @Override
+    public boolean execute() {
+        String input = Menu.scanner.nextLine();
+        input = input.replaceAll("^\\s+", "");
+        input = input.replaceAll("\\s+$", "");
+
+        Matcher helpMatcher = Menu.getMatcher(input, "^help$");
+        Matcher backMatcher = Menu.getMatcher(input, "^back$");
+        Matcher viewCategoriesMatcher = Menu.getMatcher(input, "^view categories$");
+        Matcher filteringMatcher = Menu.getMatcher(input, "^filtering$");
+        Matcher sortingMatcher = Menu.getMatcher(input, "^sorting$");
+        Matcher showProductsMatcher = Menu.getMatcher(input, "^show products$");
+        Matcher showProductMatcher = Menu.getMatcher(input, "^show product \\S+$");
+
+        try {
+            if (helpMatcher.find()) {
+                help();
+            } else if (backMatcher.find()) {
+                return false;
+            } else if (viewCategoriesMatcher.find()) {
+                context = controller.getCategories();
+            } else if (filteringMatcher.find()) {
+                new FilteringMenu(controller);
+            } else if (sortingMatcher.find()) {
+                new SortingMenu(controller);
+            } else {
+                throw new Exception("Invalid command. Use help if you haven't yet, " +
+                        "else, close the application Immediately.");
+            }
+        } catch (Exception e) {
+            error = e.toString();
+        }
+        return true;
+    }
+
+    private void showFilteredProducts(ArrayList<Product> products) {
+
+    }
+
+    private void showSpecificProduct(Product product) {
+
+    }
+}
