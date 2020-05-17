7bdc177a271bc1f1ed7851243c6a8d5692f5db4d
morabba <292.arma@gmail.com>
2020-05-17 21:46:23 +0430

    starting ProductsMenu

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -4,17 +4,10 @@ import model.Product;
 import model.Seller;
 
 public class ProductController extends UserController {
-    private static Product currentProduct;
-    public ProductController () {
-
-    }
-    public ProductController (Product currentProduct) {
-        this.currentProduct = currentProduct;
-    }
     public static String showProduct(String productId) {
         Product product = Database.getProductById(productId);
         assert product != null;
-        Seller seller = product.getSeller();
+        Seller seller = (Seller) Database.getUserById(product.getSeller().toString());
         assert seller != null;
         return "Product name:\t" + product.getName() + "\n" +
                 "Brand:\t" + product.getBrand() + "\n" +
@@ -25,4 +18,37 @@ public class ProductController extends UserController {
                 "Description:\n" + product.getDescription();
     }
 
+    //TODO complete function
+
+    public String getCategories() {
+        return "These are all categories";
+    }
+
+    public String showAvailableFilters() {
+        return "These are all filters";
+    }
+
+    public String filterProducts(String filter) {
+        return "These are products after being filtered";
+    }
+
+    public String getCurrentFilters() {
+        return "These are current filters";
+    }
+
+    public void removeFilter(String filter) throws Exception {
+        throw new Exception("willing to remove filter " + filter);
+    }
+
+    public String showAvailableSorts() {
+        return "These are all sorts";
+    }
+
+    public void setSort(String sort) throws Exception {
+        throw new Exception("sort is set to " + sort);
+    }
+
+    public String getCurrentSort() {
+        return "This is the current sort";
+    }
 }
